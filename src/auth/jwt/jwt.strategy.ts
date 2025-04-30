9171026a458804f14c74028bdca9fe37ed264537
Hothyfa Elbeera <h.s.elbeera@gmail.com>
2025-04-30 15:22:48 +0100

    jwt update

---
+++ src/auth/jwt/jwt.strategy.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@nestjs/common';
+import { PassportStrategy } from '@nestjs/passport';
+import { Strategy, ExtractJwt } from 'passport-jwt';
+import { UserService } from '../../user/user.service';
+import { JwtPayload } from './jwt-payload.interface';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor(private userService: UserService) {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      secretOrKey: process.env.JWT_SECRET || 'defaultSecret',
+    });
+  }
+
+  async validate(payload: JwtPayload) {
+    const user = await this.userService.findById(payload.userId);
+    if (!user) {
+      throw new Error('User not found');
+    }
+    return user; // Return user object to attach it to the request
+  }
+}
